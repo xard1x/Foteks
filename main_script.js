f497a0bf9c14783a7a31e2284455cb527212e4aa
xard1x <akuharenko21@gmail.com>
2025-11-16 15:14:11 +0500

    upd1.9.2

--- main_script.js
+++ main_script.js
@@ -1,16 +1,22 @@
 function file(){   
+    const button1 = document.getElementById('main_box2_2')
+    const box = document.getElementById('main_box2')
+    let txt1 = document.getElementById('main_txt1')
     let file_input = document.getElementById('real-input');
     file_input.addEventListener('change', function() {
+        const image = document.getElementById('main_img')
         let files = event.target.files
         if (files.length > 0) {
         let fileName = files[0].name;
-        console.log("Имя выбранного файла:", fileName);
+        txt1.innerHTML = "Имя выбранного файла: " + fileName;
         }
         
-        //let file_path = file_input.value
-        //console.log(file_path)
         let file = event.target.files[0];
         let fileUrl = URL.createObjectURL(file);
+        image.src = fileUrl
+        image.style.visibility = 'visible'
+        image.style.opacity = '1'
+        image.style.display = 'block'
 
         Tesseract.recognize(
         fileUrl,
@@ -20,5 +26,12 @@ function file(){
         console.log(text)
         })
     });
+
+    button1.style.visibility = 'hidden'
+    button1.style.opacity = '0'
+    button1.style.display = 'none'
+    box.style.height = '45%'
+    box.style.width = '50%'
+    
     
 } 
