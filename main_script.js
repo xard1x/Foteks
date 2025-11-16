4075162664cef72fcfbe13b66124a3db58bb0fe9
xard1x <akuharenko21@gmail.com>
2025-11-16 21:07:54 +0500

    upd1.9.3

--- main_script.js
+++ main_script.js
@@ -2,7 +2,9 @@ function file(){
     const button1 = document.getElementById('main_box2_2')
     const box = document.getElementById('main_box2')
     let txt1 = document.getElementById('main_txt1')
+    let txt2 = document.getElementById('final_text')
     let file_input = document.getElementById('real-input');
+    const box1 = document.getElementById('main_box3')
     file_input.addEventListener('change', function() {
         const image = document.getElementById('main_img')
         let files = event.target.files
@@ -17,6 +19,9 @@ function file(){
         image.style.visibility = 'visible'
         image.style.opacity = '1'
         image.style.display = 'block'
+        image.style.marginTop = '3%'
+        image.style.height = '85%'
+        image.style.width = '70%'
 
         Tesseract.recognize(
         fileUrl,
@@ -24,6 +29,7 @@ function file(){
         { logger: m => console.log(m)}
         ).then(({ data: { text } }) => {
         console.log(text)
+        txt2.innerHTML = text
         })
     });
 
@@ -31,7 +37,11 @@ function file(){
     button1.style.opacity = '0'
     button1.style.display = 'none'
     box.style.height = '45%'
-    box.style.width = '50%'
+    box.style.left = '22%'
+    box.style.alignItems = 'flex-start'
+    box1.style.visibility = 'visible'
+    box1.style.opacity = '1'
+    box1.style.display = 'block'
     
     
 } 
