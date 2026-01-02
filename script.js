3301a09a6f559f28b92427f246d1a097ac8d7db8
xard1x <akuharenko21@gmail.com>
2026-01-02 13:37:07 +0500

    upd1.11.3

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const text_1 = document.getElementById('theme_lb')
 const text_2 = document.getElementById('news_lb')
 const text_3 = document.getElementById('info_lb')
 const but1 = document.getElementById('but1')
+const box1 = document.getElementById('info_box1')
+const box2 = document.getElementById('info_box2')
+const box3 = document.getElementById('info_box3')
 
 function theme(){
     num = num * -1
@@ -18,7 +21,10 @@ function theme(){
         text_1.style.color = '#d6d6d6ff'
         text_2.style.color = '#d6d6d6ff'
         text_3.style.color = '#d6d6d6ff'
-        but1.style.backgroundColor = '#1868b8ff'
+        but1.style.backgroundColor = '#11569bff'
+        box1.style.backgroundColor = '#0d4286ff'
+        box2.style.backgroundColor = '#0e57a0ff'
+        box3.style.backgroundColor = '#2a78c5ff'
 
     }
     if(num === 1){
@@ -27,6 +33,10 @@ function theme(){
         text_1.style.color = '#000000ff'
         text_2.style.color = '#000000ff'
         text_3.style.color = '#000000ff'
+        but1.style.backgroundColor = '#1d80e3'
+        box1.style.backgroundColor = '#1662c6'
+        box2.style.backgroundColor = '#1d80e3'
+        box3.style.backgroundColor = '#4ca3fa'
     }
 
 
